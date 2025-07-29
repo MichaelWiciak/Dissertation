32649156b237897c3ac7e1f670eb6296044d94a1
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2025-07-29 22:30:30 +0800

    feat: update

--- DatasetManipulations/preprocessing.py
+++ DatasetManipulations/preprocessing.py
@@ -3,12 +3,12 @@
 # and create output column by copying the code
 # separete the data into the code, code+comments, code+comments+ast datasets
 
-#remove repo, path, func_name, language, code,  sha, url,partition, docstring, code, original_string
+# remove repo, path, func_name, language, code,  sha, url,partition, docstring, code, original_string
 # important: code_tokens, docstring_tokens
-# comments = docstring??/ i guess, no actual comments there. 
+# comments = docstring??/ i guess, no actual comments there.
 
 
-# so for input output dataset, i need to chhose a random token and make it the output? 
+# so for input output dataset, i need to chhose a random token and make it the output?
 # so i replace that token in input with <mask> and then use the output token to train the model?
 
 import sys
@@ -26,7 +26,7 @@ import esprima
 def ast_to_dict(node):
     result = {}
     for key, value in node.items():
-        if isinstance(value, dict) and 'type' in value:
+        if isinstance(value, dict) and "type" in value:
             result[key] = ast_to_dict(value)
         elif isinstance(value, list):
             result[key] = [ast_to_dict(item) for item in value]
@@ -36,11 +36,13 @@ def ast_to_dict(node):
             result[key] = value
     return result
 
+
 def checkArguments(x):
     if len(sys.argv) != x:
         print("ERROR: Incorrect number of arguments")
         print("USAGE: python preprocessing.py [path to dataset]")
         sys.exit(1)
+
 
 def checkPath():
     if not os.path.exists(sys.argv[1]):
@@ -48,12 +50,13 @@ def checkPath():
         print("USAGE: python preprocessing.py [path to dataset]")
         sys.exit(1)
 
+
 def checkArgs():
     # first check that there are 2 arguments
     checkArguments(2)
     # now check that the first argument is a valid path
     checkPath()
-    
+
 
 def main():
     # arguments should be:
@@ -84,10 +87,10 @@ def createCodeOnlyDataset(path):
 
     # now save the file
     codeOnly = codeOnlyDataset(data)
-    
+
     codeOnly = masking(codeOnly)
     saveFile(codeOnly, path, "code")
-    
+
 
 def createCodeCommentsDataset(path):
     # open the file
@@ -112,6 +115,7 @@ def createCodeCommentsASTDataset(path):
     data = removeUnnecessaryStringColumn(data)
     saveFile(data, path, "codeCommentsAST")
 
+
 def openFile(path):
     with open(path, "r") as file:
         data = file.readlines()
@@ -131,11 +135,11 @@ def masking(data):
     # comment is a token that starts with \" and ends with \"
     for i in range(len(data)):
         token = random.choice(data[i]["code_tokens"])
-        while token.startswith("\"") and token.endswith("\""):
+        while token.startswith('"') and token.endswith('"'):
             token = random.choice(data[i]["code_tokens"])
         data[i]["output"] = [token]
         data[i]["code_tokens"][data[i]["code_tokens"].index(token)] = "<mask>"
-        
+
     return data
 
 
@@ -147,7 +151,7 @@ def removeUnnecessary(data):
     for i in range(len(data)):
         data[i] = json.loads(data[i])
         data[i]["code"] = checkData(data[i]["code"], data[i]["language"])
-    
+
     # now we are left with None original_string s so we need to remove them
 
     # print the length of the data
@@ -169,15 +173,15 @@ def removeUnnecessary(data):
         data[i].pop("partition")
         data[i].pop("docstring")
         data[i].pop("original_string")
-    
+
     return data
+
 
 def checkData(original_string, language, write=False):
     with open("invalid_data.txt", "a") as file:
 
         # if it forms a valid ast, then it's good
         # if not, then remove it
-
 
         if language == "python":
             try:
@@ -207,19 +211,19 @@ def checkData(original_string, language, write=False):
             print("Invalid language")
             sys.exit(1)
     closeFile(file)
-    
+
     return original_string
 
 
 def removeUnnecessaryCodeCommentsAST(data):
-     # first check if the code forms a valid AST
+    # first check if the code forms a valid AST
     # if not, then remove it
     # then remove the unnecessary columns
 
     for i in range(len(data)):
         data[i] = json.loads(data[i])
         data[i]["code"] = checkData(data[i]["code"], data[i]["language"], True)
-    
+
     # now we are left with None original_string s so we need to remove them
     data = [x for x in data if x["code"] is not None]
 
@@ -238,6 +242,7 @@ def removeUnnecessaryCodeCommentsAST(data):
 
     return data
 
+
 def saveFile(data, path, folderName):
     # check if the directory exists, if not create it
     directory = path.split("/")
@@ -246,16 +251,18 @@ def saveFile(data, path, folderName):
     if not os.path.exists("preprocessedJS/" + directory):
         os.makedirs("preprocessedJS/" + directory)
     # now save the file, but need to put it into folders of based on folderName
-    with open("preprocessedJS/" + directory + "/" + directory + "_" + folderName + ".jsonl", "w") as file:
+    with open(
+        "preprocessedJS/" + directory + "/" + directory + "_" + folderName + ".jsonl",
+        "w",
+    ) as file:
         for i in range(len(data)):
             file.write(json.dumps(data[i]) + "\n")
 
     closeFile(file)
-    
-    
+
+
 def closeFile(file):
     file.close()
-
 
 
 def codeOnlyDataset(data):
@@ -265,24 +272,33 @@ def codeOnlyDataset(data):
         # for each code_tokens, we need to remove every token that is a comment
         # comments are a token that starts with \" and ends with \"
         # Idea: code_tokens = [token for token in code_tokens if not (token.startswith("\"") and token.endswith("\""))]
-        data[i]["code_tokens"] = [token for token in data[i]["code_tokens"] if not (token.startswith("\"") and token.endswith("\""))]
-    
+        data[i]["code_tokens"] = [
+            token
+            for token in data[i]["code_tokens"]
+            if not (token.startswith('"') and token.endswith('"'))
+        ]
+
     return data
+
 
 def codeCommentsASTOnly(data):
     # First remove the docstring_tokens column
     # Then remove the comments from the code_tokens
     for i in range(len(data)):
         data[i].pop("docstring_tokens")
-        data[i]["code_tokens"] = [token for token in data[i]["code_tokens"] if not (token.startswith("\"") and token.endswith("\""))]
+        data[i]["code_tokens"] = [
+            token
+            for token in data[i]["code_tokens"]
+            if not (token.startswith('"') and token.endswith('"'))
+        ]
 
     # Now calculate the AST for each code_tokens and append it to the data
     for i in range(len(data)):
         data[i]["ast"] = calculateAST(data[i]["code"], language="javascript")
 
     return data
-    
-    
+
+
 # Given the code tokens, calculate the abstract syntax tree
 def calculateAST(original_string, language="python"):
     # the code tokens are in a form of a list
@@ -300,7 +316,7 @@ def calculateAST(original_string, language="python"):
             print(e)
     elif language == "javascript":
         try:
-            tree = esprima.parseScript(code) 
+            tree = esprima.parseScript(code)
             tree = ast_to_dict(tree)
             # convert the AST to string
             tree = json.dumps(tree)
@@ -309,16 +325,16 @@ def calculateAST(original_string, language="python"):
             tree = None
             print("Error calculating AST for code tokens: ", code)
             print(e)
-        
-    # print("The AST is: ", ast.dump(tree))
 
+    # print("The AST is: ", ast.dump(tree))
 
     return tree
 
+
 def remove_comments_and_docstrings(code_string):
     # Tokenize the code
-    code_tokens = tokenize.tokenize(BytesIO(code_string.encode('utf-8')).readline)
-    
+    code_tokens = tokenize.tokenize(BytesIO(code_string.encode("utf-8")).readline)
+
     # Filter out comments and docstrings
     filtered_tokens = []
     in_docstring = False
@@ -327,14 +343,14 @@ def remove_comments_and_docstrings(code_string):
             in_docstring = not in_docstring
         elif not in_docstring and token.type != tokenize.COMMENT:
             filtered_tokens.append(token)
-    
+
     # Join the remaining tokens to reconstruct the code without comments and docstrings
-    cleaned_code = ''.join([token.string for token in filtered_tokens])
+    cleaned_code = "".join([token.string for token in filtered_tokens])
 
     return cleaned_code
 
+    # add two things, the average time taken for a query,
 
-    # add two things, the average time taken for a query, 
 
 if __name__ == "__main__":
     main()
